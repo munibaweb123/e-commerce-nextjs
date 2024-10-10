ac83225d47d98de20ecc73294a75d8eaeea2bec7
munibaweb123 <munibavirtualuni@gmail.com>
2024-10-10 23:51:05 +0500

    scrollbar correction

--- src/app/components/ProductSlider.tsx
+++ src/app/components/ProductSlider.tsx
@@ -21,8 +21,11 @@ interface Product {
           <div className="flex justify-evenly space-x-4">
             {products.map(product => (
               <div key={product.id} className="inline-block px-4 bg-blue-50 ">
-                <Image src={product.image1} alt={product.name} className="w-80 h-80 object-cover" height={500} width={500} /><br/>
-                <Image src={product.image2} alt={product.name} className="w-80 h-20 object-cover pr-10" height={100} width={100}/>
+           <div className="flex justify-around md:w-80 w-full h-80 object-cover">
+                <Image src={product.image1} alt={product.name}  height={500} width={500} /><br/>
+
+                </div>
+                <Image src={product.image2} alt={product.name} className="w-40 h-10 object-cover pr-10" height={200} width={200}/>
                 <h3 className="mt-2 text-2xl text-left font-bold w-100">{product.name}</h3>
                 <h4 className="mt-2 text-left font-bold w-100">{product.price}</h4>
               </div>
